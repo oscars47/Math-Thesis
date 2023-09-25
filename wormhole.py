853953cd265733cffcc68130446dad5ff9ca7131
Oscar Scholin <orsa2020@mymail.pomona.edu>
2023-09-25 03:35:58 -0700

    fixed hamiltonian

--- wormhole.py
+++ wormhole.py
@@ -13,48 +13,56 @@ def get_H(N=10, J2=2):
     '''Returns the SYK Hamiltonian for N qubits.
     NOTE: wormhole paper used N = 10, J^2 = 2, beta= 4, mu = -12
     '''
-    # initialize H
-    H = np.zeros((2**N, 2**N), dtype=np.complex128)
+
     # helper function to return product of pauli matrices given a list of indices
     def get_prod(indices):
         '''Returns the uncoupled product of majorana fermions in qubit basis'''
-        # initialize product
-        prod_tot = np.eye(2**N)
+        main_prod = np.eye(2**N)
         for ind in indices:
-            for i in range(N-1): # big product of all the terms
-                # need to take tensor product with Sz at index ind and everywhere else identity
-                prod_ind = np.eye(2**N)
-                for j in range(N): # building the tensor product term in the big product
-                    if j == 0:
-                        if i == 0: # initialize the product for this particular index
-                            prod = Sz
-                        else:
-                            prod = np.eye(2)
-                    elif j != ind:
+            for m in range(N-1): # loop over all qubits, except the last one; move the location of sigma_Z within tensor product
+                prod= np.array([1])
+                for n in range(N): # do the tensor product
+                    if n != m:
                         prod = np.kron(prod, np.eye(2))
                     else:
                         prod = np.kron(prod, Sz)
-                prod_ind = np.dot(prod_ind, prod)
-            if ind %2 != 0:
-                prod_ind = np.dot(prod_ind, np.kron(np.eye(2**(N-1)), Sx))
-            else:
-                prod_ind = np.dot(prod_ind, np.kron(np.eye(2**(N-1)), Sy))
-        
-            prod_tot = np.dot(prod_tot, prod_ind)
-        return prod_tot
+            main_prod = np.dot(main_prod, prod)
+            if ind %2 == 0:
+                # build giant tensor product with sigma_X at end and identity everywhere else
+                prod= np.array([1])
+                for n in range(N): # do the tensor product
+                    if n < N-1:
+                        prod = np.kron(prod, np.eye(2))
+                    else:
+                        prod = np.kron(prod, Sy)
+            else: # same as above, but we use a Sy instead of Sx
+                prod= np.array([1])
+                for n in range(N): # do the tensor product
+                    if n < N-1:
+                        prod = np.kron(prod, np.eye(2))
+                    else:
+                        prod = np.kron(prod, Sy)
+            # append last result to main_prod
+            main_prod = np.dot(main_prod, prod)
+        return main_prod
 
+    # initialize H
+    H = np.zeros((2**N, 2**N), dtype=np.complex128)
     # get all possible combinations of N qubits
     for i in range(N):
         for j in range(i, N, 1):
-            for l in range(max(i, j), N, 1):
-                for k in range(max(i, j, l), N, 1):
+            for l in range(j, N, 1):
+                for k in range(j, N, 1):
+                    print(i, j, l, k)
                     # determine coupling constant for this term
-                    c = np.random.normal(loc=0.0, scale=math.factorial(3)*J2/(2**(N)), size=1)
+                    c = np.random.normal(loc=0.0, scale=math.factorial(3)*J2/(2**(N)))
+                    # c=1
                     # assign matrices to each fermion in qubit basis by checking whether the index is odd or even
                     # get product of majorana fermions
                     prod_tot = get_prod([i, j, l, k])
                     # mutliply by c and add to H
                     H += c*prod_tot
+                    print(H)
     return H
 
 
@@ -63,8 +71,6 @@ if __name__ == '__main__':
     print(H)
     # confirm that H is hermitian
     print(np.allclose(H, H.conj().T))
-    print('eigenvalues')
-    print(np.linalg.eigvals(H))
 
                     
             
