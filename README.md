1beec8b6350507c23f8b840a4d77876b0b6f8547
oscars47 <orsa2020@mymail.pomona.edu>
2024-02-05 00:44:49 -0800

    partway thru protocol

--- README.md
+++ README.md
@@ -3,16 +3,25 @@ Repo for all the code used in my math thesis at Pomona College ('24) with Prof. 
 
 ## Major updates
 
-BIG PICTURE GOAL FOR END OF WEEK, 1/19/24 
-* research: have rigorous benchmarks on performance of EA2.1 and initial simulation and experiental results.
-* writing: rough draft intro chapter
+## 2/4/24
+* reorganizing syk_qk.py so that get_SYK returns the H pauli_sum_op object.
+* added get_TFD(), which depends on get_bell_pair(), time_reverse(), time_reverse_op().
+* realized need to add both the L and R definitions for the jordan-wigner because in the potential term otherwise they all become I (added param, ```left```).
+* changing from Zlokapa's definition so all values are in terms of Qiskit QC representation.
+** for expV, we use 20 trotter steps and imaginary time (so the exponent is real)
+** left the matrix versions of these in comments for future refer.
+* fixed trotter_suzuki_circuit to accept imaginary time steps
 
-## 1/18/23
+
+## 1/30/24
+* built syk_qk.py, which has the function get_SYK() to generate SYK hamiltonian for any given number of majorana fermions. automatically runs trotterization. was able to convert time evolution circuit with 20 trotter steps from 0 to 1 from about 580000 gates to 10, for an avg speedup of 58036 += 0 and avg fidelity to original of 6.087174651948914e-12 ± 1.2044172115152807e-12, for 10 hamiltonians. (built function to benchmark_SYK to automatically run these computations).
+
+## 1/18/24
 * realized making a CCNOT gate is impossible w EA2.1 bc it only allows CNOT between neighbors. making a convolutional transformer to decompose circuit structure for N<=10 qubits.
 
 * sphynx_gen.py creates the random matrices and decoded structure to be used in training; sphynx.py will have the actual code for the transformer.
 
-## 1/17/23
+## 1/17/24
 * Was able to run SWAP, CSWAP. not able to solve CCNOT or CCCNOT. why? let's debug this.
 
 * hide print statements
@@ -44,11 +53,11 @@ To do:
 
  * also separate issue with CZ: realized that the current framework of EA2.1 does not allow simultaneous operations. fixed this by changing the framework to account for operations to be applied in simultaneous moments. 
 
-## 1/15/23
+## 1/15/24
 Realized Qiskit has a module, transpile, which can convert an arbitrary unitary into decomposed circuit. BUT: much less efficient in terms of number of gates used compared to my algorithm: using a random_circuit(3,5) EA2.1 used ~6 gates whereas Qiskit transpile used ~60-100. Need rigorous comparison
 
 
-### 1/13/23
+### 1/13/24
 Troubleshooting model 0; it doesn't seem possible to simplify as you go, so will create model 2.1 which essentially does model 2 but then finds all the stretches of RxRyRzP and simplifies after the fact. Implemented this. Increased num times to run trabbit from 1 to 5.
 
 Benchmarking model 2 first: tested on depth of 5 up to 6 qubits, up to depth of 15 on 3 qubits. Handles great on my laptop and will run rigrous test with model 2.1 simplification when I get back from Germany. 
@@ -57,14 +66,14 @@ Realized major impracticality: adds more RP blocks even if no CNOTs: only add if
 
 Reminder: all in elegans_adapt2.py
 
-### 1/9/23
+### 1/9/24
 Fixed issue with sample circuits 4-9. Model 2 can now effectively handle [some rotation] CNOT [some rotation] and CNOT [rotation] in any order on the qubits. Will implement simplification on each individual RP section tomorrow.
  
 
-### 1/6/23
+### 1/6/24
 Wrote new Circuit class in circuit.py to handle storage, updating of genes and conversion to unitary. Implemented in elegans_adapt2.py for the iterative learning circuit. Started test_circuits.py which is a framework for simulating the circuits and actually experimentally testing them using Qiskit.
 
-### 1/3/23
+### 1/3/24
 Developed "learning circuit" in elegans_new.py using the Pauli spin, P, CNOT. Also built a genetic algorithm in ga.py. Adapting elegans_new.py to cirq in elegans_cirq.py to better study the circuits with CNOTs--since if we do not allow any CNOTs, a circuit of only 4N for N qubits can learn a circuit of arbitrary depth.
 
 
--- syk_qk.py
+++ syk_qk.py
@@ -6,6 +6,7 @@ from qiskit.opflow import I, X, Y, Z, PauliSumOp, PauliOp
 import matplotlib.pyplot as plt
 import os, time
 from math import factorial
+from scipy.linalg import expm
 
 ## ------ visualization ------- ##
 def print_matrix(matrix, show=False, save=False, save_name=None):
@@ -48,7 +49,94 @@ def num_gates_pso(pauli_sum_op):
         gate_count += np.sum(pauli_string != 'I')
 
     return gate_count
-## ------ simulation ------- ##
+
+#### ------ WORMHOLE PROTOCOL ------- ####
+def majorana_to_qubit_op(l,  num_qubits, left=True):
+    '''Performs Jordan Wigner transformation from Majorana to qubit operators, as in Jafferis and Gao et al.
+
+    Params:
+        l (int): index of Majorana operator
+        num_qubits (int): number of qubits
+        left (bool): whether the Majorana operator is on the left or right side 
+    
+    '''
+    j = (l + 1) // 2
+    
+    # start with identity operator for all qubits
+    operator_chain = [I] * num_qubits
+    
+    # apply (ZX)^(j-1)
+    for idx in range(j - 1):
+        operator_chain[idx] = Z if idx % 2 == 0 else X
+    
+    if left:
+        # apply the XX or YX at j-th position
+        if l % 2 == 0:  # for 2j-1, we apply XX, which is just X because X^2 = I
+            operator_chain[j - 1] = X
+        else:  # for 2j, we apply YX, which simplifies to iZ
+            operator_chain[j - 1] = Y  # Apply Y at the j-th position
+
+         # if not the last qubit, apply X on the next qubit for the 2j-1 case
+        if l % 2 == 0 and j < num_qubits:  # check if there's a qubit to apply X on
+            operator_chain[j] = X
+
+    else:
+        # apply I j-th position
+        operator_chain[j - 1] = I
+
+        # if not the last qubit, apply Y if odd or Z if even on the next qubit for the 2j-1 case
+        if l % 2 == 0 and j < num_qubits:
+            operator_chain[j] = Z
+        elif l % 2 == 1 and j < num_qubits:
+            operator_chain[j] = Y
+
+    # convert the list of single-qubit operators to a PauliOp
+    full_operator = operator_chain[0]
+    for op in operator_chain[1:]:
+        full_operator = full_operator ^ op
+
+    return full_operator
+
+def get_SYK(n_majorana, J=2, left=True):
+    '''Returns the SYK Hamiltonian as a PauliSumOp object in Qiskit.
+
+    Params:
+        N (int): number of Majorana fermions
+        J (float): effective variance
+        save (bool): whether to save the matrix representation of the circuit
+        save_name (str): name of the file to save as
+    
+    '''
+
+    print(f'Generating SYK Hamiltonian for N_m = {n_majorana}...')
+
+    # Parameters
+    n_qubits = n_majorana // 2
+    var = factorial(3) * J**2 / (n_majorana**3)
+    J = np.random.normal(0, np.sqrt(var), (n_majorana, n_majorana, n_majorana, n_majorana))
+
+    # Ensure Jijkl is antisymmetric
+    for i in range(n_majorana):
+        for j in range(n_majorana):
+            for k in range(n_majorana):
+                for l in range(n_majorana):
+                    if i >= j or j >= k or k >= l:
+                        J[i, j, k, l] = 0                     
+
+    # initialize Hamiltonian as all 0s
+    H = PauliSumOp.from_list([("I" * n_qubits, 0.0)]) 
+
+    for i in range(n_majorana):
+        for j in range(n_majorana):
+            for k in range(n_majorana):
+                for l in range(n_majorana):
+                    if J[i, j, k, l] != 0:
+                        term = majorana_to_qubit_op(i, n_qubits, left=left) @ majorana_to_qubit_op(j, n_qubits, left=left) @ majorana_to_qubit_op(k, n_qubits, left=left) @ majorana_to_qubit_op(l, n_qubits, left=left)
+                        H += J[i, j, k, l] * term
+
+    return H
+
+## ------ TROTTER-SUZUKI ------- ##
 def trotter_suzuki_circuit(pauli_sum_op, time, steps):
     '''Perform the Trotter-Suzuki approximation of the time evolution operator.'''
     n_qubits = pauli_sum_op.num_qubits
@@ -59,7 +147,11 @@ def trotter_suzuki_circuit(pauli_sum_op, time, steps):
     for _ in range(steps):
         for term in pauli_sum_op:
             # print('term:', term.primitive.coeffs[0] )
-            angle = -1j * np.abs(term.primitive.coeffs[0]) * delta_t # sometimes Qiskit will autoconvert YX = iZ thus adding a phase, so we need to take the abs
+            # need to check if delta_t is real or imaginary
+            if np.isreal(delta_t):
+                angle = -1j * np.abs(term.primitive.coeffs[0]) * delta_t # sometimes Qiskit will autoconvert YX = iZ thus adding a phase, so we need to take the abs
+            else:
+                angle = np.abs(term.primitive.coeffs[0]) * delta_t
             angle = np.imag(angle)
             # print('angle:', angle)
             pauli_strings = term.primitive.paulis.to_labels()
@@ -76,112 +168,11 @@ def trotter_suzuki_circuit(pauli_sum_op, time, steps):
                         # 'I' does nothing
     return qc
 
-def get_SYK(n_majorana, J=2, save=False, save_name=None):
-    '''Returns the SYK Hamiltonian as a PauliSumOp object in Qiskit.
-
-    Params:
-        N (int): number of Majorana fermions
-        J (float): effective variance
-        save (bool): whether to save the matrix representation of the circuit
-        save_name (str): name of the file to save as
-    
-    '''
-
-    # Parameters
-    n_qubits = n_majorana // 2
-    var = factorial(3) * J**2 / (n_majorana**3)
-    J = np.random.normal(0, np.sqrt(var), (n_majorana, n_majorana, n_majorana, n_majorana))
-
-    # Ensure Jijkl is antisymmetric
-    for i in range(n_majorana):
-        for j in range(n_majorana):
-            for k in range(n_majorana):
-                for l in range(n_majorana):
-                    if i >= j or j >= k or k >= l:
-                        J[i, j, k, l] = 0
-
-    # def majorana_to_qubit_op(i, n_qubits): ## OLD, wrong
-    #     '''Map Majorana operator to qubit operator.'''
-    #     qubit_index = i // 2
-    #     operator = I ^ qubit_index  # Identity on all qubits before the target
-    #     operator = operator ^ (X if i % 2 == 0 else Y)  # X or Y on the target qubit
-    #     operator = operator ^ (I ^ (n_qubits - qubit_index - 1))  # Identity on all qubits after the target
-    #     return operator
-                        
-    # def majorana_to_qubit_op(l, num_qubits): ## OLD, not accouting for 
-    #     '''Performs Jordan Wigner transformation from Majorana to qubit operators, as in Jafferis and Gao et al.
-
-    #     Note: `l` is the index of the Majorana operator in terms of the total majorana number, and `num_qubits` is the number of qubits in the system.
-        
-        
-    #     '''
-        
-    #     # Initialize the operator as an identity for each qubit
-    #     operator_chain = [I] * num_qubits
-
-    #     # Determine the position of the final X or Y
-    #     position = l // 2
-
-    #     # Construct the alternating sequence of Z and X up to the position
-    #     for i in range(position):
-    #         operator_chain[i] = Z if i % 2 == 0 else X
-
-    #     # Set the final X or Y at the position
-    #     operator_chain[position] = X if l % 2 == 0 else Y
-
-    #     # Create the full operator
-    #     full_operator = operator_chain[0]
-    #     for op in operator_chain[1:]:
-    #         full_operator = full_operator ^ op
-
-    #     return full_operator
-                        
-                        
-    def majorana_to_qubit_op(l,  num_qubits):
-        '''Performs Jordan Wigner transformation from Majorana to qubit operators, as in Jafferis and Gao et al. Assumes only left subsystem expression since one value of H_L,R is what's quoted in Jafferis et al wormhole paper.'''
-        j = (l + 1) // 2
-        
-        # start with identity operator for all qubits
-        operator_chain = [I] * num_qubits
-        
-        # apply (ZX)^(j-1)
-        for idx in range(j - 1):
-            operator_chain[idx] = Z if idx % 2 == 0 else X
-        
-        # apply the XX or YX at j-th position
-        if l % 2 == 0:  # for 2j-1, we apply XX, which is just X because X^2 = I
-            operator_chain[j - 1] = X
-        else:  # for 2j, we apply YX, which simplifies to iZ
-            operator_chain[j - 1] = Y  # Apply Y at the j-th position
-
-        # if not the last qubit, apply X on the next qubit for the 2j-1 case
-        if l % 2 == 0 and j < num_qubits:  # check if there's a qubit to apply X on
-            operator_chain[j] = X
-
-        # convert the list of single-qubit operators to a PauliOp
-        full_operator = operator_chain[0]
-        for op in operator_chain[1:]:
-            full_operator = full_operator ^ op
-
-        return full_operator
-
-    # initialize Hamiltonian as all 0s
-    H = PauliSumOp.from_list([("I" * n_qubits, 0.0)]) 
-
-    for i in range(n_majorana):
-        for j in range(n_majorana):
-            for k in range(n_majorana):
-                for l in range(n_majorana):
-                    if J[i, j, k, l] != 0:
-                        term = majorana_to_qubit_op(i, n_qubits) @ majorana_to_qubit_op(j, n_qubits) @ majorana_to_qubit_op(k, n_qubits) @ majorana_to_qubit_op(l, n_qubits)
-                        H += J[i, j, k, l] * term
-    
-    # convert to unitary time evolution
-    tf = 1.0
-    steps = 20
+def time_evolve(H, tf=1, steps=20, benchmark=False, save=False, save_name=None):
+    '''Time evolves the Hamiltonian H from t0 to tf in steps.'''
+    print('Time evolving...')
+    # get the circuit 
     H_circ = trotter_suzuki_circuit(H, tf, steps)
-
-    # print('H_circ:', H_circ)
 
     # log the total number of gates in the qc
     num_gates_initial_dict = H_circ.count_ops()
@@ -190,52 +181,190 @@ def get_SYK(n_majorana, J=2, save=False, save_name=None):
 
     # run transpiler
     H_opt_circ = transpile(H_circ, optimization_level=1)
+    print('Transpiled...')
 
     # log the number of gates in the optimized qc
     num_gates_opt_dict = H_opt_circ.count_ops()
     num_gates_opt = sum(num_gates_opt_dict.values())
     print('Number of gates in optimized circuit:', num_gates_opt)
 
-    # compute fidelity
-    # get matrix representation of the circuit
-    H_circ_matrix = Operator(H_circ).data
-    H_opt_circ_matrix = Operator(H_opt_circ).data
+    if benchmark:
+        # compute fidelity
+        # get matrix representation of the circuit
+        H_circ_matrix = Operator(H_circ).data
+        print('Converted to matrix')
+        H_opt_circ_matrix = Operator(H_opt_circ).data
 
-    # get eigenvalues and eigenvectors of optimized circuit
-    eigvals, eigvecs = np.linalg.eig(H_opt_circ_matrix)
-    print('Eigenvalues:', eigvals)
+        # fidelity
+        fidelity = np.linalg.norm(H_circ_matrix - H_opt_circ_matrix)
 
-    # fidelity
-    fidelity = np.linalg.norm(H_circ_matrix - H_opt_circ_matrix)
+        # log gate speedup
+        gate_speedup = num_gates_initial / num_gates_opt
 
-    # log gate speedup
-    gate_speedup = num_gates_initial / num_gates_opt
+        print('Fidelity:', fidelity)
+        print('Gate speedup:', gate_speedup)
 
-    print('Fidelity:', fidelity)
-    print('Gate speedup:', gate_speedup)
+        # print out both matrices
+        print_matrix(H_circ_matrix, save=True, save_name='H_circ')
+        print_matrix(H_opt_circ_matrix, save=True, save_name='H_opt_circ')
 
-    # print out both matrices
-    print_matrix(H_circ_matrix, save=True, save_name='H_circ')
-    print_matrix(H_opt_circ_matrix, save=True, save_name='H_opt_circ')
+        if save:
+            # add to 'syk_qiskit' folder
+            path = os.path.join(os.getcwd(), 'syk_qiskit')
+            if not os.path.exists(path):
+                os.makedirs(path)
+            # timestamp
+            if save_name is None:
+                np.save(os.path.join(path, f'H_circ_{time.time()}'), H_circ_matrix)
+                np.save(os.path.join(path, f'H_opt_circ_{time.time()}'), H_opt_circ_matrix)
+            else:
+                np.save(os.path.join(path, save_name+f'H_circ_{time.time()}'), H_circ_matrix)
+                np.save(os.path.join(path, save_name+f'H_opt_circ_{time.time()}'), H_opt_circ_matrix)
 
-    if save:
-        # add to 'syk_qiskit' folder
-        path = os.path.join(os.getcwd(), 'syk_qiskit')
-        if not os.path.exists(path):
-            os.makedirs(path)
-        # timestamp
-        if save_name is None:
-            np.save(os.path.join(path, f'H_circ_{time.time()}'), H_circ_matrix)
-            np.save(os.path.join(path, f'H_opt_circ_{time.time()}'), H_opt_circ_matrix)
-            np.save(os.path.join(path, f'eigvals_{time.time()}'), eigvals)
-            np.save(os.path.join(path, f'eigvecs_{time.time()}'), eigvecs)
-        else:
-            np.save(os.path.join(path, save_name+f'H_circ_{time.time()}'), H_circ_matrix)
-            np.save(os.path.join(path, save_name+f'H_opt_circ_{time.time()}'), H_opt_circ_matrix)
-            np.save(os.path.join(path, save_name+f'eigvals_{time.time()}'), eigvals)
-            np.save(os.path.join(path, save_name+f'eigvecs_{time.time()}'), eigvecs)
+        return fidelity, gate_speedup
+    else:
+        return H_opt_circ # just return the optimized circuit
 
-    return fidelity, gate_speedup
+## ------ TFD and helper functions adapted from Zlokapa's thesis ------- ##
+# def get_TFD(H, beta=4):
+#     '''Takes in pauli sum op and returns the TFD state. Assumes time reversal applied first.
+
+#     Params:
+#         H (PauliSumOp): Hamiltonian
+#         beta (float): inverse temperature
+    
+#     '''
+#     # get the matrix representation of the Hamiltonian
+#     H_mat = H.to_matrix()
+#     N = int(np.log2(H_mat.shape[0]))
+
+#     expH = expm(-beta * H_mat/4)
+
+#     # apply time reversal
+#     tfd = time_reverse(expH@get_bell_pair(N), right=True)
+
+#     # get parition function to normalize
+#     Z = np.sqrt(np.vdot(tfd, tfd))
+
+#     return tfd / Z
+
+# def get_bell_pair(N):
+#     '''Returns the bell state in N qubit hilbert space'''
+#     zero =np.array([1, 0])
+#     one = np.array([0, 1])
+
+#     bell_pair = (np.kron(zero, zero) + np.kron(one, one)) * 1/np.sqrt(2)
+
+#     # now put inside N qubit hilbert space
+#     if N==2:
+#         return bell_pair
+#     else:
+#         epr = bell_pair
+#         for _ in range(N//2-1):
+#             epr = np.kron(bell_pair, epr)
+
+#         return epr
+    
+# def time_reverse(M, right=True):
+#     '''Calls time_reverse_op on matrix M'''
+#     N = int(np.log2(M.shape[0]))
+#     m = time_reverse_op(N, right)
+#     return m @ np.conjugate(M)
+
+# def time_reverse_op(N, right=True):
+#     '''Returns the time reversal operator for N qubits'''
+#     Sy = np.array([[0, -1j], [1j, 0]]) # Pauli Y
+
+#     mr = np.kron(np.identity(1), -1j*Sy)
+#     ml = np.kron(-1j*Sy, np.identity(1))
+
+#     print(mr, ml)
+
+#     if right:
+#         m = mr
+#     else:
+#         m = ml
+
+#     for _ in range(N-2):
+#         if right:
+#             m = np.kron(m, mr)
+#         else:
+#             m = np.kron(m, ml)
+
+#     return m
+    
+def get_TFD(H, beta=4, steps=20):
+    '''Takes in pauli sum op and returns the TFD state. Assumes time reversal applied first. Performs computation as quantum circuit. Applies Trotter-Suzuki with imag time evolve the Hamiltonian.
+
+    Params:
+        H (PauliSumOp): Hamiltonian
+        beta (float): inverse temperature
+        steps (int): number of steps for Trotter-Suzuki
+        '''
+
+    # exponentiate using trotter suzuki
+    tfd_i = trotter_suzuki_circuit(H, -1j*beta, steps)
+    print(tfd_i)
+
+    H_exp = -1j*beta/4 * H
+
+    # use trotter suzuki to get the circuit
+    tfd = time_evolve(H_exp, steps)
+
+    return tfd # is already normalized bc quantum circuit
+
+def get_bell_pair(N):
+    '''Returns the bell state in N qubit hilbert space using quantum circuit'''
+    qc = QuantumCircuit(N)
+    qc.h(0)
+    qc.cx(0, 1)
+    return qc
+    
+## ------ Apply potential ------- ##
+def get_expV(n_majorana, mu =-12, steps=20):
+    '''Computes the exponential of the potential V for n_majorana fermions for interaction param mu'''
+
+    n_qubits = n_majorana // 2
+
+    # initialize the potential as pauli sum op
+    V = PauliSumOp.from_list([("I" * n_qubits, 0.0)])
+    
+    for j in range(n_majorana):
+        term = majorana_to_qubit_op(j, n_qubits, left=True) @ majorana_to_qubit_op(j, n_qubits, left=False)
+        V += 1/(4*n_majorana) * term
+
+    # scale by mu
+    V = mu * V
+
+    # compute exponential with trotter suzuki
+    expV = time_evolve(V, tf=1, steps=steps)
+    return expV
+
+## ------ main function to implment the protocol ------- ##
+def implement_protocol(n_majorana, mu=-12, beta=4):
+    '''Computes the correlation K for n_majorana fermions at time t, interaction param mu, and inverse temperature beta'''
+
+    # get the Hamiltonian
+    H = get_SYK(n_majorana) # pauli sum op
+
+    # make tfd
+    tfd = get_TFD(H, beta) # quantum circuit
+
+    # get bell pair P and Q
+    bell_pair = get_bell_pair(n_majorana) # quantum circuit
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 ## ------- testing ------- ##
 def benchmark_SYK(num, n_majorana):
@@ -243,7 +372,7 @@ def benchmark_SYK(num, n_majorana):
     fidelities = []
     gate_speedups = []
     for _ in range(num):
-        fidelity, gate_speedup = get_SYK(n_majorana)
+        fidelity, gate_speedup = time_evolve(get_SYK(n_majorana), benchmark=True)
         fidelities.append(fidelity)
         gate_speedups.append(gate_speedup)
     avg_fidelity = np.mean(fidelities)
@@ -256,5 +385,8 @@ def benchmark_SYK(num, n_majorana):
 
 if __name__ == "__main__":
     # print(time.time())
-    get_SYK(20, save=True, save_name='SYK_20')
+    # get_SYK(20, save=True, save_name='SYK_20')
     # benchmark_SYK(10, 20) # N = 10 qubits
+    H = get_SYK(10)
+    tfd = get_TFD(H)
+    print(tfd)
